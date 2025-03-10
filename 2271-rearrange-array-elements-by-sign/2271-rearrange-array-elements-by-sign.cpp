0bf1f72fed7e2b72743b89d4bd8f7a15eebff13e
Rishit Aggarwal <rishitaggarwal1@gmail.com>
2025-03-10 22:53:07 +0530

    Time: 5 ms (70.47%), Space: 128.4 MB (42.42%) - LeetHub

---
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n,0);
+        int posI=0,negI=1;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>=0)
+            {
+                ans[posI]=nums[i];
+                posI+=2;
+            }
+            else
+            {
+                ans[negI]=nums[i];
+                negI+=2;
+            }
+        }
+        return ans;
+    }
+};

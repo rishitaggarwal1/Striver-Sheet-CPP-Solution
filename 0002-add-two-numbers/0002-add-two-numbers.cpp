8520ba45f4e05304be8c8f6d16443d7809a6b8d0
Rishit Aggarwal <rishitaggarwal1@gmail.com>
2025-03-12 02:03:17 +0530

    Time: 2 ms (40.53%), Space: 77.2 MB (43.18%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,66 +11,21 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans=NULL;
-        ListNode* pnt=ans;
-        int carry=0;
-        while(l1!=NULL && l2!=NULL)
-        {
-            int x=l1->val+l2->val+carry;
-            carry=x/10;
-            ListNode *temp=new ListNode();
-            temp->val=x%10;
-            if(ans==NULL)
-            {    ans=temp; pnt=ans; }
-            else
-            {
-                pnt->next=temp;
-                pnt=pnt->next;
-            }
-            l1=l1->next;
-            l2=l2->next;
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* curr = dummyHead;
+        int carry = 0;
+        while (l1 != NULL || l2 != NULL || carry != 0) {
+            int x = l1 ? l1->val : 0;
+            int y = l2 ? l2->val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
         }
-        while(l1!=NULL)
-        {
-            int x=l1->val+carry;
-            carry=x/10;
-            ListNode *temp=new ListNode();
-            temp->val=x%10;
-            if(ans==NULL)
-            {    
-                ans=temp; 
-                pnt=ans; 
-            }
-            else
-            {
-                pnt->next=temp;
-                pnt=pnt->next;
-            }
-            l1=l1->next;
-        }
-        while(l2!=NULL)
-        {
-            int x=l2->val+carry;
-            carry=x/10;
-            ListNode *temp=new ListNode();
-            temp->val=x%10;
-            if(ans==NULL)
-            {    
-                ans=temp; 
-                pnt=ans; 
-            }
-            else
-            {
-                pnt->next=temp;
-                pnt=pnt->next;
-            }
-            l2=l2->next;
-        }
-        if(carry>0)
-        {
-            ListNode* temp=new ListNode(carry);
-            pnt->next=temp;
-        }
-        return ans;
+        ListNode* result = dummyHead->next;
+        delete dummyHead;
+        return result;
     }
 };

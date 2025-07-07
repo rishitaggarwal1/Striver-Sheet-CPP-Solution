0f13b1e21779818d0a27a08c61080b8007e65fe1
Rishit Aggarwal <rishitaggarwal1@gmail.com>
2025-07-07 22:41:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prefix to Postfix Conversion/prefix-to-postfix-conversion.cpp
@@ -0,0 +1,31 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    string preToPost(string pre_exp) {
+        stack<string> st;
+        int n=pre_exp.length();
+        for(int i=n-1;i>=0;i--)
+        {
+            char c=pre_exp[i];
+            if((c>='a' && c<='z')||(c>='A' && c<='Z')||(c>='0' && c<='9'))
+            {
+                string s="";
+                s.push_back(c);
+                st.push(s);
+            }
+            else 
+            {
+                string s1=st.top();
+                st.pop();
+                string s2=st.top();
+                st.pop();
+                s1=s1;
+                s1+=s2;
+                s1.push_back(c);
+                st.push(s1);
+            }
+        }
+        return st.top();
+    }
+};

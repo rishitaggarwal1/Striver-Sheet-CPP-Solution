ef46f86b045959f65079605e3863209ba033150a
Rishit Aggarwal <rishitaggarwal1@gmail.com>
2025-09-05 23:15:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Beauty of All Substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,28 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int beautySum(string s) {
+        // Your code goes here
+        int ans=0;
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            unordered_map<char,int> m;
+            int a=ans;
+            for(int j=i;j<n;j++)
+            {
+                m[s[j]]++;
+                int mx=INT_MIN, mm=INT_MAX;
+                for(auto x:m)
+                {
+                    if(x.second>mx)  mx=x.second;
+                    if(x.second<mm)  mm=x.second;
+                }
+                ans+=(mx-mm);
+            }
+            if(a==ans)  break;
+        }
+        return ans;
+    }
+};

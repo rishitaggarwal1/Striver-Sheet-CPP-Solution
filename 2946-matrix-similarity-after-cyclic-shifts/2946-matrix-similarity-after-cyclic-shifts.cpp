6532857e0c8fdd1b61cf39ef4159359f6802eb97
Rishit Aggarwal <rishitaggarwal1@gmail.com>
2026-03-27 11:26:14 +0530

    Time: 0 ms (100%), Space: 31.2 MB (67.7%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -5,21 +5,11 @@ public:
         k = k%n;
         for(int i=0;i<m;i++)
         {
-            vector<int> v = mat[i];
-            if(i%2==0)
+            for(int j=0;j<n;j++)
             {
-                reverse(mat[i].begin(),mat[i].begin()+k);
-                reverse(mat[i].begin()+k,mat[i].end());
-                reverse(mat[i].begin(),mat[i].end());
-            }
-            else
-            {
-                int d = n-k;
-                reverse(mat[i].begin(),mat[i].begin()+d);
-                reverse(mat[i].begin()+d,mat[i].end());
-                reverse(mat[i].begin(),mat[i].end());
-            }
-            if(v!=mat[i]) return false;  
+                if(mat[i][j]!=mat[i][(j+k)%n])
+                    return false;
+            }  
         }
         return true;
     }
